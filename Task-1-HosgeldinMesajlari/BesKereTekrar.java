910c71b7be89fbfbcc3a0526e66cd5074c0d07c1
250542008 <250542008@firat.edu.tr>
2025-10-22 19:43:29 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,12 +1,10 @@
-public class Main {
-
+public class BeşTekrar {
     public static void main(String[] args) {
-        System.out.println("Java'ya Hoşgeldin");
-        System.out.println("Java'ya Hoşgeldin");
-        System.out.println("Java'ya Hoşgeldin");
-        System.out.println("Java'ya Hoşgeldin");
-        System.out.println("Java'ya Hoşgeldin");
-
+        System.out.println("Java'ya Hoşgelfiniz");
+        System.out.println("Java'ya Hoşgelfiniz");
+        System.out.println("Java'ya Hoşgelfiniz");
+        System.out.println("Java'ya Hoşgelfiniz");
+        System.out.println("Java'ya Hoşgelfiniz");
     }
-
 }
+
