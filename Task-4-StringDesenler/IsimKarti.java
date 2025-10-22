aa991b0f911061e732e41c21b7f120981922b07f
250542008 <250542008@firat.edu.tr>
2025-10-22 19:50:16 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Mustafa";
+        String soyad = "Şahıngoz";
+        String ogrNo = "250542008";
+        String bolum = "Yazilim Muh.";
+
+        System.out.println("======================");
+        System.out.println("OGRENCI BILGILERI");
+        System.out.println("======================");
+        System.out.println("Ad      :  " + ad);
+        System.out.println("Soyad    :  " + soyad);
+        System.out.println("Alan    :  " + ogrNo);
+        System.out.println("Bolum    :  " + bolum);
+        System.out.println("======================");
+    }
+}
