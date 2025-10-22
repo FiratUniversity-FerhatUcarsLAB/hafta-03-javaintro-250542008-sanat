d2fbaeb77f447fb4d11ae30611767c1edc55e686
250542008 <250542008@firat.edu.tr>
2025-10-22 19:46:02 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,16 @@
+public class IkiIslem {
+    public static void main(String[] args) {
+        //Pay hesapla
+        double pay = (9.5*4.5) - (2.5*3);
+
+        //Payda hesapla
+        double payda = 45.5 - 3.5;
+
+        //Bolme yap
+        double sonuc = pay / payda;
+
+        //Formatla ve yazdır
+        System.out.printf("%.4f\n", sonuc);
+
+    }
+}
