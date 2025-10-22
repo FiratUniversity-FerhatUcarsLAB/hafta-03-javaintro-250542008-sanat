bc517d1591f001a37f0b74b11ac375963a351bfa
250542008 <250542008@firat.edu.tr>
2025-10-22 19:49:51 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,14 @@
+public class HesapÖzeti {
+    public static void main(String[] args) {
+        System.out.println("| ÜRÜN    |  FİYAT(TL)  |     MİKTAR|");
+        System.out.println("|_______________________________________|");
+        System.out.println("|süt      |   35        |         1L    |");
+        System.out.println("|ekmek    |   15        |      200gr    |");
+        System.out.println("|pirinç   |  145        |        1Kg    |");
+        System.out.println("|elma     |   70        |        1Kg    |");
+        System.out.println("|mandalina|   15        |      0.5Kg    |");
+        System.out.println("|çikolta  |   25        |       1adet   |");
+        System.out.println("=========================================");
+
+    }
+}
