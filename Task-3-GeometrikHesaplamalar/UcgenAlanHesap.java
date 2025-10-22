19acc523be73cc7aecf1bf9d2f61148c4c732f69
250542008 <250542008@firat.edu.tr>
2025-10-22 19:48:33 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,12 @@
+public class UcgenAlanHesap {
+    static void main(String[] args) {
+        double taban = 11.3;
+        double yükseklik = 15.71;
+
+        //Alan hesapla
+        double alan = 11.3*15.71/2;
+
+        //Sonucları yazdir
+        System.out.println("Alan: " + alan);
+    }
+}
