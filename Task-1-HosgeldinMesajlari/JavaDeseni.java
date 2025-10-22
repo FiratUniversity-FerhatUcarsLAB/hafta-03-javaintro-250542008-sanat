f33a9238bfcfb9dff66236d585dcd4582763b283
250542008 <250542008@firat.edu.tr>
2025-10-22 15:34:37 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoşgeldin");
+        System.out.println("Java'ya Hoşgeldin");
+        System.out.println("Java'ya Hoşgeldin");
+        System.out.println("Java'ya Hoşgeldin");
+        System.out.println("Java'ya Hoşgeldin");
+
+    }
+
+}
